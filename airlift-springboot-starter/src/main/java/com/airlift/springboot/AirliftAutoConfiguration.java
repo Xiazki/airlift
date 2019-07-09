9f90da4191f11fc425da3dd2d38ffb2ea1100780
xiaxiang <xiaxianginw@163.com>
2019-07-09 17:05:06 +0800

    修改 server 启动条件

--- airlift-springboot-starter/src/main/java/com/airlift/springboot/AirliftAutoConfiguration.java
+++ airlift-springboot-starter/src/main/java/com/airlift/springboot/AirliftAutoConfiguration.java
@@ -4,6 +4,7 @@ import com.airlift.spring.AirliftProperties;
 import com.airlift.spring.ClientBeanPostProcessor;
 import com.airlift.spring.ServerStarter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +20,8 @@ public class AirliftAutoConfiguration {
     }
 
     @Bean
-    public ServerStarter serverStarter(){
+    @ConditionalOnProperty(prefix = "airlift.server", name = "enable", havingValue = "true")
+    public ServerStarter serverStarter() {
         return new ServerStarter();
     }
 
